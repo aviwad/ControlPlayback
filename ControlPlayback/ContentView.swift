96fcba5f62b4e8084f491469d79f1044284b43cf
Avi Wadhwa <aviwad@gmail.com>
2025-12-24 15:51:31 -0800

    Initial Commit

---
+++ ControlPlayback/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ControlPlayback
+//
+//  Created by Avi Wadhwa on 2025-12-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ControlPlayback/ControlPlaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlPlaybackApp.swift
+//  ControlPlayback
+//
+//  Created by Avi Wadhwa on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct ControlPlaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
